079c82ff07713ef137da16f2782f25ebc1f651b6
oshadaera68 <mindartlk010@gmail.com>
2024-12-02 06:57:22 +0530

    updated content

--- oop_CLI/src/Customer.java
+++ oop_CLI/src/Customer.java
@@ -3,5 +3,29 @@
  * Version: v0.1.0
  **/
 
-public class Customer {
+public class Customer implements Runnable {
+    private final int customerRetrivalRate;
+    private final TicketPool ticketPool;
+
+    public Customer(int customerRetrivalRate, TicketPool ticketPool) {
+        this.customerRetrivalRate = customerRetrivalRate;
+        this.ticketPool = ticketPool;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(1000);
+                for (int i = 0; i < customerRetrivalRate; i++) {
+                    Ticket ticket = ticketPool.removeTicket(); // removing the tickets
+                    if (ticket == null) {
+                        return;
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- oop_CLI/src/Ticket.java
+++ oop_CLI/src/Ticket.java
@@ -4,4 +4,47 @@
  **/
 
 public class Ticket {
+    // Fields:- ticketId, event, price
+    private int ticketId;
+    private String event;
+    private double price;
+
+    public Ticket(int ticketId, String event, double price) {
+        this.ticketId = ticketId;
+        this.event = event;
+        this.price = price;
+    }
+
+    public int getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(int ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "ticketId='" + ticketId + '\'' +
+                ", event='" + event + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
