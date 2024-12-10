bd9215be39bf800e6a52af2a4b6784268b47267d
oshadaera68 <mindartlk010@gmail.com>
2024-12-10 13:48:19 +0530

    reset the codes

--- oop_CLI/src/Configuration.java
+++ oop_CLI/src/Configuration.java
@@ -1,11 +1,8 @@
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Serializable;
+import java.io.*;
+import java.util.Scanner;
 
 /**
  * Coded By: Era Boy
@@ -13,28 +10,26 @@ import java.io.Serializable;
  **/
 
 public class Configuration implements Serializable {
-    public int totalTickets;
-    public int ticketReleaseRate;
-    public int customerRetrievalRate;
-    public int maxTicketCapacity;
+    int totalTickets;
+    int ticketReleaseRate;
+    int customerRetrievalRate;
+    int maxTicketCapacity;
 
-    /**
-     * Loads configuration from a JSON file.
-     */
+    // Save configuration to a file in JSON format
+    public void saveConfiguration(String filePath) throws IOException {
+        Gson gson = new Gson();
+        try (FileWriter writer = new FileWriter(filePath)) {
+            gson.toJson(this, writer);
+        }
+    }
+
+    // Load configuration from a JSON file
     public static Configuration loadConfiguration(String filePath) throws IOException {
         Gson gson = new Gson();
         try (FileReader reader = new FileReader(filePath)) {
             return gson.fromJson(reader, Configuration.class);
-        }
-    }
-
-    /**
-     * Saves the current configuration to a JSON file.
-     */
-    public void saveConfiguration(String filePath) throws IOException {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        try (FileWriter writer = new FileWriter(filePath)) {
-            gson.toJson(this, writer);
+        } catch (JsonSyntaxException e) {
+            throw new IOException("Invalid JSON format in configuration file.", e);
         }
     }
 }
--- oop_CLI/src/ValidationRule.java
+++ oop_CLI/src/ValidationRule.java
@@ -3,10 +3,7 @@
  * Version: v0.1.0
  **/
 
-// Interface for validation rules
-// This interface defines a contract for validation logic.
-// Implementing classes or lambda expressions will provide the specific validation logic.
+//  Interface for validation rules
 public interface ValidationRule {
-    // Method to check if a given value satisfies the validation rule
     boolean isValid(int value);
 }
