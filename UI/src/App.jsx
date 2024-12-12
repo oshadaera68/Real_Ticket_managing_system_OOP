f3fc255771d5d4b8a6b2f5358f521c017fd9457d
oshadaera68 <mindartlk010@gmail.com>
2024-12-12 10:01:45 +0530

    changed the variable names and updated the code base

---
+++ UI/src/App.jsx
@@ -0,0 +1,16 @@
+
+import TicketManager from './components/TicketManager';
+import ConfigurationManager from './components/ConfigurationManager';
+
+function App() {
+
+  return (
+    <div>
+    <h1>Ticket Management System</h1>
+    <TicketManager />
+    <ConfigurationManager />
+</div>
+  )
+}
+
+export default App
